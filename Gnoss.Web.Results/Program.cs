06e2d4c4d267b8e4cdbab4ad87e7ca2287a0e025
Javier-RSP <javierruiz@gnoss.com>
2026-04-20 12:48:08 +0200

    Security Improvement

--- Gnoss.Web.Results/Program.cs
+++ Gnoss.Web.Results/Program.cs
@@ -9,19 +9,21 @@ namespace Gnoss.Web.Results
 {
     public class Program
     {
+        private static Serilog.ILogger _startupLogger;
         public static void Main(string[] args)
         {
-            LoggingService.ConfigurarBasicStartupSerilog().CreateBootstrapLogger();
+            _startupLogger = LoggingService.ConfigurarBasicStartupSerilog().CreateBootstrapLogger().ForContext<Program>();
             try
             {
                 CreateHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "Error fatal durante el arranque");
+                _startupLogger.Fatal(ex, "Error fatal durante el arranque");
             }
             finally
             {
+                (_startupLogger as IDisposable)?.Dispose();
                 Log.CloseAndFlush(); // asegura que se escriben todos los logs pendientes
             }
         }
@@ -30,10 +32,14 @@ namespace Gnoss.Web.Results
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostContext, config) =>
                 {
-                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                    config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    LoggingService.ConfigurarSeguimientoFicheros(hostContext, config, _startupLogger);
                 })
                 .UseSerilog((context, services, configuration) => LoggingService.ConfigurarSerilog(context.Configuration, services, configuration))
+                .ConfigureServices((context, services) =>
+                {
+                    LoggingService.SuscribirCambios(context, _startupLogger);
+                    _startupLogger.Information("Suscripción a cambios de configuración registrada");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
